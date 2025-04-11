09ef48b4b34a14b862feb82fe01586984d241f01
Shayla <Shaylahunt834@gmail.com>
2025-04-11 15:07:01 -0500

    u

---
+++ Static_Hunt_Shayla/Calculate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Hunt_Shayla
+{
+    static class Calculate
+    {
+
+        public static float result = 0.0f;
+
+        static Calculate()
+        {
+            result = 0.0f;
+        }
+
+        // Intergers being added, subtracted, multiplied, and divided and getting a result.
+        public static int Add (int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtract(int x, int y) 
+        {
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y) 
+        {
+            return x * y;
+        }
+        public static int Divide(int x, int y) 
+        {
+            return x / y;
+        }
+
+        //Float being added, subtracted, multiplied, and divided and getting a result.
+
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+
+        public static float Subtract(float x, float y)
+        {
+            return x - y;
+        }
+
+        public static float Multiply(float x, float y)
+        {
+            return x * y;
+        }
+        public static float Divide(float x, float y)
+        {
+            return x / y;
+        }
+    }
+}
--- Static_Hunt_Shayla/Program.cs
+++ Static_Hunt_Shayla/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Calls the executes each method in the Calculate class with integer values.
+            Console.WriteLine("Integer Methods");
+            Console.WriteLine("5 + 10 = " + Calculate.Add(5, 10));
+            Console.WriteLine("4 - 9 = " + Calculate.Subtract(4, 9)); 
+            Console.WriteLine("12 * 7 = " + Calculate.Multiply(12, 7)); 
+            Console.WriteLine("3 / 8 = " + Calculate.Divide(3, 8));
+
+            // Calls the executes each method in the Calculate class with float values.
+            Console.WriteLine("\nFloat methods");
+            Console.WriteLine("5.65 + 10.732 = " + Calculate.Add(5.65f, 10.732f));
+            Console.WriteLine("4.9 - 9.25 = " + Calculate.Subtract(4.9f, 9.25f));
+            Console.WriteLine("12.82 * 7.19 = " + Calculate.Multiply(12.82f, 7.19f));
+            Console.WriteLine("3.003 / 8.59 = " + Calculate.Divide(3.003f, 8.59f));
         }
     }
 }
